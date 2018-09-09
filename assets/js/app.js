b9152230822ad5b9029e3695c95c3c464b1fde25
lawrence <l@lawluk.com>
2018-09-08 23:52:08 -0700

    big update for new ghost

--- assets/js/app.js
+++ assets/js/app.js
@@ -53,6 +53,16 @@
 		$(window).resize(updateAllImages);
 	});
 
+	// https://github.com/TryGhost/Casper/pull/475/files#diff-ce5e030f2e2e2a50f18199464f07ea70
+	var images = document.querySelectorAll('.kg-gallery-image img');
+  images.forEach(function (image) {
+      var container = image.closest('.kg-gallery-image');
+      var width = image.attributes.width.value;
+      var height = image.attributes.height.value;
+      var ratio = width / height;
+      container.style.flex = ratio + ' 1 0%';
+  });
+
 	/* Init Everything Together (to use with smoothstate callbacks) */
 	function initEverything() {
 		updateAllImages();
